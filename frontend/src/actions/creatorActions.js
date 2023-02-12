1fce1e287913e911eb863ff92ac9e85e5ce2ba46
Kiera Thomas <kiera.c.thomas@vanderbilt.edu>
2023-02-12 15:03:28 -0600

    did POST reqs for login and order attempts, including auth fail errors

---
+++ frontend/src/actions/creatorActions.js
@@ -0,0 +1,50 @@
+import {CREATOR_LIST_REQUEST, 
+    CREATOR_LIST_SUCCESS, 
+    CREATOR_LIST_FAIL,
+
+    CREATOR_DETAILS_REQUEST, 
+    CREATOR_DETAILS_SUCCESS, 
+    CREATOR_DETAILS_FAIL,
+
+} from '../constants/creatorConstants'
+import axios from 'axios'
+
+export const listCreators = () => async (dispatch) => { 
+try{
+    dispatch({ type: CREATOR_LIST_REQUEST })
+    const { data } = await axios.get('/api/creators/')
+    dispatch({
+        type:CREATOR_LIST_SUCCESS, 
+        payload: data
+    })
+
+} catch(error){
+    dispatch({
+        type: CREATOR_LIST_FAIL,
+        payload: error.response && error.response.data.detail 
+            ? error.response.data.detail
+            : error.message,
+    })
+
+}
+}
+
+export const listCreatorDetails = (id) => async (dispatch) => { 
+try{
+    dispatch({ type: CREATOR_DETAILS_REQUEST })
+    const { data } = await axios.get(`/api/creators/${id}`)
+    dispatch({
+        type:CREATOR_DETAILS_SUCCESS, 
+        payload: data
+    })
+
+} catch(error){
+    dispatch({
+        type: CREATOR_DETAILS_FAIL,
+        payload: error.response && error.response.data.detail
+            ? error.response.data.detail
+            : error.message,
+    })
+
+}
+}
