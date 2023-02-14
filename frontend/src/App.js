469a732c7c76047ed3bfbb2f17d71626920bae13
johannwest7012 <johannwest@Johanns-MacBook-Air.local>
2023-02-13 22:46:15 -0600

    added proper node_modules and did routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+
+//import { Container } from 'react-bootstrap'
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+//import Header from './components/Header'
+//import Footer from './components/Footer'
+
+import HomeScreen from './screens/HomeScreen';
+import CreatorScreen from './screens/CreatorScreen';
+import FavoritesScreen from './screens/FavoritesScreen'
+import LoginScreen from './screens/LoginScreen'
+import RegisterScreen from './screens/RegisterScreen'
+import ProfileScreen from './screens/ProfileScreen'
+import PortfolioScreen from './screens/PortfolioScreen'
+
+
+
+
+import ParticleBackground from './components/ParticleBackground';
+//import ParticleBackgroundDark from './components/ParticleBackgroundDark';
+
+import WithNav from './components/WithNav'
+import WithoutNav from './components/WithoutNav';
+import history from './history';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <Router history={history}>
+          <Routes>
+            <Route element={<WithoutNav />}>
+                <Route path='/' element={<ParticleBackground />} exact />
+            </Route>
+            <Route element={<WithNav />}>
+                <Route path='/home' element={<HomeScreen />} exact />
+            </Route>
+            <Route element={<WithNav />}>
+                <Route path='/login' element={<LoginScreen />} />
+            </Route>
+            <Route element={<WithNav />}>
+                <Route path='/register' element={<RegisterScreen />} />
+            </Route>
+            <Route element={<WithNav />}>
+                <Route path='/profile' element={<ProfileScreen />} />
+            </Route>
+            <Route element={<WithNav />}>
+                <Route path='/creator/:id' element={<CreatorScreen animate={true} />} />
+            </Route>
+            <Route element={<WithNav />}>
+                <Route path='/favorites/:id?' element={<FavoritesScreen animate={true} />} />
+            </Route>
+            <Route element={<WithNav />}>
+                <Route path='/portfolio' element={<PortfolioScreen />} />
+            </Route>
+            
+          </Routes>
+    </Router>
+    
   );
 }
 
---
+++ frontend/src/components/ParticleBackground.js
@@ -0,0 +1,44 @@
+
+import { useCallback } from "react";
+import Particles from "react-particles";
+import { loadFull } from "tsparticles";
+import particlesConfig from "./config/particles-config";
+import {Card, Row, Col, Image, Button, Container, Stack} from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+
+import myLogo from './CreateX-red.svg'
+
+const ParticleBackground = () => {
+
+    const particlesInit = useCallback(async engine => {
+        console.log(engine);
+        // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+        // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+        // starting from v2 you can add only the features you need reducing the bundle size
+        await loadFull(engine);
+    }, []);
+
+    const particlesLoaded = useCallback(async container => {
+        await console.log(container);
+    }, []);
+
+
+    return (
+    <div>
+    <Particles params={particlesConfig} init={particlesInit} loaded={particlesLoaded}/>
+        <Row>
+            <Col></Col>
+            <Col>
+                <Stack gap={0}>
+                    <Image width='700' src={myLogo}/>
+                    <Link to='/home' className='btn btn-dark my-3'>Enter</Link>
+                </Stack>
+            </Col>
+            <Col></Col>
+        </Row>
+
+    </div>
+    )
+}
+
+export default ParticleBackground
