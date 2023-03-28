79c5eab3ec6b3b61a4d3ec04a488f0d3ed96b656
Johann West <johannwest@gmail.com>
2023-03-27 22:19:56 -0500

    Create secrets.py

---
+++ backend/backend/secrets.py
@@ -0,0 +1,10 @@
+#GET CRUNKED FOOL 
+
+SDJANGO_KEY = 'thissiasillykey'
+
+SNAME =  'thissiasillykey'
+SUSER =  'thissiasillykey'
+SPASSWORD =  'thissiasillykey'
+SHOST =  'thissiasillykey'
+SAWS_ACCESS_KEY_ID =  'thissiasillykey'
+SAWS_SECRET_ACCESS_KEY =  'thissiasillykey'
