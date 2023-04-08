582d57f4ee64ce824ebf5c07346790b6e1ada812
Johann West <johannwest@gmail.com>
2023-04-08 16:33:47 -0500

    commit

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -25,9 +25,11 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
 
-    path('', TemplateView.as_view(template_name = 'index.html')),
 
     path('admin/', admin.site.urls),
+
+    path('', TemplateView.as_view(template_name = 'index.html')),
+
 
     path('api/users/', include('base.urls.user_urls')),
     path('api/creators/', include('base.urls.creator_urls')),
