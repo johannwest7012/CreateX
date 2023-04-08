6e52b8040ca307c8bfa7851643d994ad06b465ec
Johann West <johannwest@gmail.com>
2023-04-08 17:45:55 -0500

    fixed debug condition

--- backend/settings.py
+++ backend/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False 
+DEBUG = True
 SECRET_KEY = os.environ.get('DJANGO_KEY')
 NAME = os.environ.get('DBNAME')
 USER = os.environ.get('USER')
