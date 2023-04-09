d6428d7b2f7701b9f8d2ddf03265e45dd427ca23
Johann West <johannwest@gmail.com>
2023-04-08 19:02:44 -0500

    awgae

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -15,5 +15,5 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 application = get_wsgi_application()
 
-from whitenoise.django import DjangoWhiteNoise
+from whitenoise import DjangoWhiteNoise
 application = DjangoWhiteNoise(application)
