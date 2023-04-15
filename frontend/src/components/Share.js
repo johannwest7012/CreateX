c4ac593c6b0df8fe4c65a654efa12a5967cf1180
Johann West <johannwest@gmail.com>
2023-04-15 17:41:37 -0500

    added useeffect to Share.js

--- frontend/src/components/Share.js
+++ frontend/src/components/Share.js
@@ -1,16 +1,26 @@
-import React from 'react'
-import { Card } from 'react-bootstrap'
+import React, {useState, useEffect} from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import { Card } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
+import { listCreators } from '../actions/creatorActions.js'
+
 
 
 function Share({ share }) {
+  const dispatch = useDispatch()
+
   console.log("ID: " + share.creator)
 
   const creatorList = useSelector(state => state.creatorList)
   const {error, loading, creators} = creatorList
 
   const creator_obj = creators.find(creator => creator._id === share.creator)
+
+  useEffect(() => {
+    if(creators.length === 0){
+        dispatch(listCreators())
+    }
+  }, [dispatch, creators])
 
   return (
     <Card className='my-3 p-3 rounded'>
